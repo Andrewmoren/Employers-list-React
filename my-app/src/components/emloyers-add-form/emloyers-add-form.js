2c39d2f9083a19712d3492692688ad383912e65b
Andrew <andrewmoren93@gmail.com>
2022-07-17 17:12:30 +0300

    add forms

--- my-app/src/components/emloyers-add-form/emloyers-add-form.js
+++ my-app/src/components/emloyers-add-form/emloyers-add-form.js
@@ -1,27 +1,53 @@
+import { Component } from "react";
+
 import "./emloyers-add-form.css";
 
-const EmployerAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Добавьте нового сотрудника</h3>
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="Как его зовут?"
-        />
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="З/П в $?"
-        />
+class EmployerAddForm extends Component {
+  constructor(props) {
+    super(props);
 
-        <button type="submit" className="btn btn-outline-light">
-          Добавить
-        </button>
-      </form>
-    </div>
-  );
-};
+    this.state = {
+      name: "",
+      salary: "",
+    };
+  }
+
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  render() {
+    const { name, salary } = this.state;
+    return (
+      <div className="app-add-form">
+        <h3>Добавьте нового сотрудника</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="Как его зовут?"
+            name="name"
+            value={name}
+            onChange={this.onValueChange}
+          />
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="З/П в $?"
+            name="salary"
+            value={salary}
+            onChange={this.onValueChange}
+          />
+
+          <button type="submit" className="btn btn-outline-light">
+            Добавить
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default EmployerAddForm;
